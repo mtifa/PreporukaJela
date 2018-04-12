69ac6c77d1a220c9ddd030eabea0413129123165
Mijo Lucic <mijolucic@hotmail.com>
2018-04-12 10:53:44 +0200

    Add project files.

---
+++ PreporukaJela/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreporukaJela.Startup))]
+namespace PreporukaJela
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
